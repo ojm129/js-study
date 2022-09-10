f9de194336c4de6dcfcbfbf66bbc736f7527852a
ojm129 <ojm129@naver.com>
2022-09-11 03:28:40 +0900

    2nd

--- js/background.js
+++ js/background.js
@@ -7,5 +7,5 @@ const images = [
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
-bgImage.src = `./img/${chosenImage}`;
+bgImage.src = `img/${chosenImage}`;
 document.body.appendChild(bgImage);
